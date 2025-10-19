fdfdf29fddc01a81f6b98ae5f3ffc3e23078ff8e
Hasna_challa <challahasna@gmail.com>
2025-10-19 16:44:51 +0200

    Create a file MySQLServer.py that creates the database alx_book_store in my MySQL server.

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Kriyayoga123+"
+    )
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error connecting to MySQL server: {e}")
+
+
+mycursor.close()
+mydb.close()
